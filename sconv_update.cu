0e2b1f168c5b3143ef1f270c57f0829493f51e39
Jokeren <robinho364@gmail.com>
2016-10-07 11:32:06 +0800

    add update

--- sconv_update.cu
+++ sconv_update.cu
@@ -1,6 +1,6 @@
 #include "sconv.h"
 
-std::string name = "sconv_update_C128_K128";
+std::string kernel_name = "sconv_update_C128_K128";
 
 bool update(const float *I, float *F, const float *O,
   unsigned int N, unsigned int C, unsigned int K,
@@ -22,20 +22,21 @@ bool update(const float *I, float *F, const float *O,
   unsigned int grid_Q = 1;
   unsigned int grid_PQ = grid_P * grid_Q;
   unsigned int grid_PQM = grid_PQ * M;
-
+  // input
   WN = W * N;
   HW = H * W;
   HWN = H * WN;
   DHW = D * HW;
   DHWN = D * HWN;
+  // filter
   RS = R * S;
   RST = T * RS;
   CRST = C * RS;
-
+  // output
   QN = Q * N;
   PQN = P * QN;
   MPQN = M * PQN;
-
+  // magic numbers
   magic32(CRST, RST, magic_RST, shift_RST);
   magic32(RST + 32, RS, magic_RS, shift_RS);
   magic32(RS + 32, S, magic_S, shift_S);
@@ -43,52 +44,49 @@ bool update(const float *I, float *F, const float *O,
   magic32(HW, W, magic_W, shift_W);
   magic32(grid_PQM, grid_PQ, magic_PQu, shift_PQu);
   magic32(grid_PQ, grid_Q, magic_Qu, shift_Qu);
-
+  // test param set up
   float *test_param;
-  cudaError_t result;
-  result = cudaMalloc((void**)&test_param, sizeof(float) * 1024);
+  cudaError_t cuda_error;
+  cuda_error = cudaMalloc((void**)&test_param, sizeof(float) * 1024);
   cudaMemset(test_param, 0, sizeof(float) * 1024);
-  if (result != cudaSuccess) {
-    std::cerr << "cuda malloc error!" << std::endl;
-    exit(1);
-  }
   void *args[43] = {&test_param, &I, &O, &F, &alpha,
-    &N, &K, &D, &H, &W, &WN, &HWN, &DHWN, &C, &CRST,
-    &RST, &magic_RST, &shift_RST, &RS, &magic_RS, &shift_RS, &S, &magic_S, &shift_S,
-    &pad_d, &pad_h, &pad_w, &str_d, &str_h, &str_w,
+    &N, &K, &D, &H, &W, &WN, &HWN, &DHWN,
+    &C, &CRST,
+    &RST, &magic_RST, &shift_RST,
+    &RS, &magic_RS, &shift_RS,
+    &S, &magic_S, &shift_S,
+    &pad_d, &pad_h, &pad_w,
+    &str_d, &str_h, &str_w,
     &P, &Q, &PQ, &QN, &PQN, &MPQN,
-    &magic_Qu, &shift_Qu, &magic_PQu, &shift_PQu,
+    &magic_Qu, &shift_Qu,
+    &magic_PQu, &shift_PQu,
     &grid_P, &grid_Q, &grid_PQ};
-
   int gridX = grid_PQM;
   int gridY = CRST / 128 + (CRST % 128 != 0);
   int gridZ = N / 64 + (N % 64 != 0);
-
-  CUresult res = cuLaunchKernel(nervana_kernels[name], gridX, gridY, gridZ, 256, 1, 1, R * S * T * 4 * 2, 0, args, NULL);
+  CUresult res = cuLaunchKernel(nervana_kernels[kernel_name], gridX, gridY, gridZ, 256, 1, 1,
+    R * S * T * 4 * 2, 0, args, NULL);
   if (res != CUDA_SUCCESS) {
-    std::cerr << "Error launching kernel " << name << " " << res << std::endl;
+    std::cerr << "Line " << __LINE__ << " error launching kernel " << kernel_name << " " << res << std::endl;
     return false;
   }
   cuCtxSynchronize();
-  //float* h_test = (float *)malloc(sizeof(float) * 64);
-  //for (int i = 0; i < 64; ++i) {
-  //  std::cout << h_test[i] << " ";
-  //}
-  //std::cout << std::endl;
-  //result = cudaMemcpy(h_test, test_param, sizeof(float) * 64, cudaMemcpyDeviceToHost);
-  //if (result != cudaSuccess) {
-  //  std::cout << result << std::endl;
-  //  std::cerr << "memcpy error!" << std::endl;
-  //}
-
-  //for (int i = 0; i < 64; ++i) {
-  //  std::cout << h_test[i] << " ";
-  //}
-
-  //free(h_test);
-
-  //std::cout << std::endl;
-
+  float* h_test = (float *)malloc(sizeof(float) * 64);
+  for (int i = 0; i < 64; ++i) {
+    std::cout << h_test[i] << " ";
+  }
+  std::cout << std::endl;
+  cuda_error = cudaMemcpy(h_test, test_param, sizeof(float) * 64, cudaMemcpyDeviceToHost);
+  if (cuda_error != cudaSuccess) {
+    std::cerr << "Line " << __LINE__ << " memcpy error: " << cuda_error << std::endl;
+    exit(1);
+  }
+  for (int i = 0; i < 64; ++i) {
+    std::cout << h_test[i] << " ";
+  }
+  std::cout << std::endl;
+  // free test_param
+  free(h_test);
   return true;
 }
 
@@ -99,6 +97,7 @@ int main() {
   unsigned int str_d = 1, str_h = 1, str_w = 1;
   unsigned int pad_d = 0, pad_h = 0, pad_w = 0;
   unsigned int M, P, Q;
+  cudaError_t cuda_error;
   M = (D - T + 2 * pad_d) / str_d + 1;
   P = (H - R + 2 * pad_h) / str_h + 1;
   Q = (W - S + 2 * pad_w) / str_w + 1;
@@ -111,40 +110,43 @@ int main() {
     h_I[i] = 1;
   }
   float* h_F = (float *)malloc(sizeof(float) * K * R * S * T);
+  // device memory
   cudaMalloc((void**)&d_I, sizeof(float) * N * C * D * H * W);
-  cudaMalloc((void**)&d_F, sizeof(float) * K * R * S * T);
+  cudaMalloc((void**)&d_F, sizeof(float) * C * R * S * T * K);
   cudaMalloc((void**)&d_O, sizeof(float) * K * M * P * Q * N);
+  // memcpy h_I, h_O
   cudaMemcpy(d_I, h_I, sizeof(float) * N * C * D * H * W,
     cudaMemcpyHostToDevice);
   cudaMemcpy(d_O, h_O, sizeof(float) * N * M * P * Q * K,
     cudaMemcpyHostToDevice);
-
+  // load kernels 
+  if (!load_kernels("./")) {
+    std::cerr << "Couldn't load all kernels" << std::endl;
+    exit(1);
+  }
+  // launch kernel
   if (!update(d_I, d_F, d_O, N, C, K, D, H, W, R, S, T, M, P, Q, str_d, str_h, str_w, pad_d, pad_h, pad_w)) {
     std::cerr << "Launch error" << std::endl;
   }
-
+  // output
   std::cout << "result" << std::endl;
-
-  cudaError_t result = cudaMemcpy(h_F, d_F, sizeof(float) * C * K * R * S * T, cudaMemcpyDeviceToHost);
-  if (result != cudaSuccess) {
-    std::cout << result << std::endl;
-    std::cerr << "memcpy error!" << std::endl;
+  cuda_error = cudaMemcpy(h_F, d_F, sizeof(float) * C * R * S * T * K, cudaMemcpyDeviceToHost);
+  if (cuda_error != cudaSuccess) {
+    std::cerr << "Line " << __LINE__ << " memcpy error: " << cuda_error << std::endl;
+    exit(1);
   }
-
-  for (int i = 0; i < 10; ++i) {
+  for (int i = 0; i < 128; ++i) {
     std::cout << h_F[i] << " ";
   }
-  
   std::cout << std::endl;
-
+  // free memory
   free(h_O);
   free(h_I);
   free(h_F);
   cudaFree(d_I);
   cudaFree(d_F);
   cudaFree(d_O);
-
+  // run successfully
   std::cout << "finish" << std::endl;
-
   return 0;
 }
